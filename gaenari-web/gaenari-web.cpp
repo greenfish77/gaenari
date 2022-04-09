b659a84fe1820575613ae35a60f806351df2fbc4
greenfish <greenfish77@gmail.com>
2022-04-09 15:04:49 +0900

    in dev.

--- gaenari-web/gaenari-web.cpp
+++ gaenari-web/gaenari-web.cpp
@@ -1,6 +1,22 @@
 #include <stdio.h>
-#include <cpp-httplib/httplib.h>
+#include "cpp-httplib/httplib.h"
+#include "gaenari/gaenari.hpp"
 
+// $ cmake ..
+// $ cmake --build . --config release
+// $ cmake --install . --prefix install
+// after running the cmake process,
+// just run ./install/bin/gaenari-web to start the web server.
+// edit ./install/bin/web-root/properties.txt to change the directory.
+//
+// the properties.txt search order:
+//  - ${target binary directory}/properties.txt
+//  - ${target binary directory}/web-root/properties.txt
+//
+// you can easily develop with the following settings:
+//   dir_www  = <gaenari_repo_dir>/gaenari-web/web-root/www
+//   dir_data = <some temp directory>
+//
 int main(void) {
 	// HTTP
 	httplib::Server svr;
