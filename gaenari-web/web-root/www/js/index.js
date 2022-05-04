030ce764cc3c3ecc4233dd69b31757b21a5c2685
greenfish <greenfish77@gmail.com>
2022-05-05 08:48:29 +0900

    web

--- gaenari-web/web-root/www/js/index.js
+++ gaenari-web/web-root/www/js/index.js
@@ -1,5 +1,7 @@
 
+// ready.
 $(function () {
+	/*
 	$.ajax({
 		url: '/api/v1/report',
 		type: 'GET',
@@ -13,12 +15,25 @@ $(function () {
 	}).fail((jqXHR, textStatus, errorThrown) => {
 	}).always((param1, param2, param3) => {
 	});
+	*/
 
 	$.ajax({
 		url: '/api/v1/project/name',
 		type: 'GET',
 	}).done((data, textStatus, jqXHR) => {
 		$('#project_name').html(data.project_name);
+	}).fail((jqXHR, textStatus, errorThrown) => {
+	}).always((param1, param2, param3) => {
+	});
+
+	$.ajax({
+		url: '/api/v1/global',
+		type: 'GET',
+	}).done((data, textStatus, jqXHR) => {
+		$('#instance_count').html(data.instance_count);
+		$('#updated_instance_count').html(data.updated_instance_count);
+		$('#instance_accuracy').html(data.instance_accuracy);
+		$('#instance_accuracy_progress').width(`${data.instance_accuracy*100}%`);
 	}).fail((jqXHR, textStatus, errorThrown) => {
 	}).always((param1, param2, param3) => {
 	});
