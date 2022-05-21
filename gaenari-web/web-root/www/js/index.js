eeda529546e2b4a97160b9953a1f38bc147057ce
greenfish <greenfish77@gmail.com>
2022-05-21 09:53:20 +0900

    web

--- gaenari-web/web-root/www/js/index.js
+++ gaenari-web/web-root/www/js/index.js
@@ -37,4 +37,21 @@ $(function () {
 	}).fail((jqXHR, textStatus, errorThrown) => {
 	}).always((param1, param2, param3) => {
 	});
+
+	$('.connectedSortable').sortable({
+		placeholder: 'sort-highlight',
+		connectWith: '.connectedSortable',
+		handle: '.card-header, .nav-tabs',
+		forcePlaceholderSize: true,
+		zIndex: 999999
+	});
+	$('.connectedSortable .card-header').css('cursor', 'move');
+
+	// jQuery UI sortable for the todo list
+	$('.todo-list').sortable({
+		placeholder: 'sort-highlight',
+		handle: '.handle',
+		forcePlaceholderSize: true,
+		zIndex: 999999
+	});
 });
